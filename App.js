9756accc3456c1d9c9b9bfc07db275fdc98529e9
fatima0773 <fatima.z0773@gmail.com>
2022-12-19 21:07:30 +0500

    final changes made

--- App.js
+++ App.js
@@ -3,22 +3,8 @@ import React, {useEffect, useState} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Splash from './src/splash/splash';
-import OnBoarding from './src/onBoarding/onBoarding';
-import SignIn from './src/login/signIn';
-import ForgotPassword from './src/login/forgotPassword';
-import NewPassword from './src/login/newPassword';
 
-import WelcomeLogin from './src/welcome/welcome_login';
-import SignUp from './src/signup/signup';
-import SignupDetails from './src/signup/signupDetails';
-import TermsAndCondition from './src/signup/termsAndConditions';
-import OtpScreen from './src/signup/optScreen';
-import WelcomeSignup from './src/welcome/welcome_signup';
 import RootNavigator from './src/navigation/rootNavigator';
-import InternalProfileView from './src/userProfile/internalProfileView';
-import EditProfile from './src/userProfile/editProfile';
-import PrivateModal from './src/userProfile/privateModal';
-import NewPost from './src/post/newPost';
 
 const App = () => {
   const [showSplash, setShowSplash] = useState(true);
@@ -40,14 +26,7 @@ const App = () => {
   if (isFirstLaunch == null && showSplash) {
     return <Splash />;
   } else {
-    return (
-      // <NewPost />
-      // <CameraPost />
-      // <PrivateModal />
-      // <EditProfile />
-      // <InternalProfileView />
-      <RootNavigator />
-    );
+    return <RootNavigator />;
   }
 };
 
